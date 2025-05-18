c814c80ee2cb7d0ff41a3db4b6fb43b1f06320b3
show098822@gmail.com <show098822@gmail.com>
2025-05-18 17:15:53 +0200

    update files

---
+++ scripts/update_publications.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import json
+from scholarly import scholarly
+
+GSCHOLAR_ID = "0gl__ukAAAAJ"
+
+def fetch_publications():
+    author = scholarly.search_author_id(GSCHOLAR_ID)
+    author = scholarly.fill(author, sections=['publications'])
+    pubs = []
+    for p in author['publications']:
+        entry = scholarly.fill(p)
+        pubs.append({
+            'authors': entry.get('bib', {}).get('author', ''),
+            'year': entry.get('bib', {}).get('year', ''),
+            'title': entry.get('bib', {}).get('title', ''),
+            'journal': entry.get('bib', {}).get('venue', ''),
+            'url': entry.get('pub_url', '#'),
+            'citations': entry.get('num_citations', 0)
+        })
+    return pubs
+
+if __name__ == "__main__":
+    data = {'publications': fetch_publications()}
+    with open('publications.json', 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=2)
